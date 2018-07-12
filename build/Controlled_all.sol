29e73d4325ff83d20cc99cb19782fa063df8b8c6
wimel <angel.contreras.mejias@gmail.com>
2018-07-12 14:14:57 +0200

    traducciónComentariosControlled_all.sol

--- build/Controlled_all.sol
+++ build/Controlled_all.sol
@@ -3,16 +3,16 @@
 pragma solidity ^0.4.18;
 
 contract Controlled {
-    /// @notice The address of the controller is the only address that can call
-    ///  a function with this modifier
+    /// @notice La dirección del controlador es la única dirección que puede
+    ///  llamar a una función con este modificador
     modifier onlyController { require(msg.sender == controller); _; }
 
     address public controller;
 
     function Controlled() public { controller = msg.sender;}
 
-    /// @notice Changes the controller of the contract
-    /// @param _newController The new controller of the contract
+    /// @notice Cambia el controlador del contrato
+    /// @param _newController El nuevo controlador del contrato
     function changeController(address _newController) public onlyController {
         controller = _newController;
     }
